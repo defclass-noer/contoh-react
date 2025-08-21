4972d86c888fed1d942c50bf1ac0454c1143066d
defclass-noer <gamenoer88@gmail.com>
2025-08-21 21:27:20 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Register from "./Register"
+import Login from "./Login"
+
+function App() {
+  return (
+    <div>
+      <h1>Supabase Auth Example</h1>
+      <Register />
+      <Login />
+    </div>
+  )
+}
+
+export default App
