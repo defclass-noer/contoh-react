be6638a180e91936f8b2cafee7b8632b5cb62294
defclass-noer <gamenoer88@gmail.com>
2025-08-23 18:02:42 +0700

    Delete src directory

--- src/supabaseClientDAT.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL_DAT;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY_DAT;
-
-export const supabaseDat = createClient(supabaseUrl, supabaseAnonKey);
